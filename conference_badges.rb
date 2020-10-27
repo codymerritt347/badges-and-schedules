dfc439f13295dc11c22642c4c0f30aa7ea0b0c57
Cody Merritt <cody.merritt@me.com>
2020-10-27 16:06:15 -0400

    commit tree bc442390d2de0a6c614f0904f932bd519fa3f94f

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    new_array = []
+    array.each do |name|
+        new_array << badge_maker(name)
+    end
+    new_array
+end
+
+def assign_rooms(array)
+    new_array = []
+    num = 1
+    array.each do |name|
+        new_array << "Hello, #{name}! You'll be assigned to room #{num}!"
+        num += 1
+    end
+    new_array
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |badge|
+        puts "#{badge}"
+    end
+    assign_rooms(array).each do |assignment|
+        puts "#{assignment}"
+    end
+end
